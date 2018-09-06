2be860f9721e0a2df8cfc5b2119653068d3b3256
mats-ken <matsken.cpp@gmail.com>
2018-09-06 14:27:09 +0900

    Added      dec-hex-oct-bin-conv.xlsx Added      for_github/add_all_files.sh Added      for_github/all_clone.bat Added      for_github/all_pull.bat Added      for_github/chg_account.bat Added      for_github/chg_account.sh Added      for_github/chg_account_local.sh Added      for_github/ignore_dotgit.sh Added      for_github/newline_code.sh Added      lib/octave/diff.m Added      lib/octave/histogram.m Added      lib/octave/ma.m Added      lib/octave/ma2.m Added      lib/octave/mah.m Added      pre-revprop-change.bat

---
+++ for_github/all_clone.bat
@@ -0,0 +1,12 @@
+git clone https://mats-ken@github.com/mats-ken/csv_view
+git clone https://mats-ken@github.com/mats-ken/draw_tool
+git clone https://mats-ken@github.com/mats-ken/dyn_test_c
+git clone https://mats-ken@github.com/mats-ken/etc
+git clone https://mats-ken@github.com/mats-ken/line_number
+git clone https://mats-ken@github.com/mats-ken/linux_tools
+git clone https://mats-ken@github.com/mats-ken/name_image
+git clone https://mats-ken@github.com/mats-ken/ohmawari_johsha
+git clone https://mats-ken@github.com/mats-ken/sudoku
+git clone https://mats-ken@github.com/mats-ken/trim_image
+
+pause
