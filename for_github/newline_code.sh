452be52e479400479c6ef3a30ba304b2428b4b13
mats-ken <matsken.cpp@gmail.com>
2018-09-21 19:25:23 +0900

    Modified   for_github/newline_code.sh

--- for_github/newline_code.sh
+++ for_github/newline_code.sh
@@ -1,16 +1,16 @@
 cd `dirname $0`
 
 
-find . -type f -iname "*.vcxproj*" | xargs unix2dos
+find . -type f -iname "*.vcxproj*" | xargs unix2dos -q
 
 for EXT in sln csv bat
 do
-  find . -type f -iname "*.$EXT" | xargs unix2dos
+  find . -type f -iname "*.$EXT" | xargs unix2dos -q
 done
 
 
 
 for EXT in sh
 do
-  find . -type f -iname "*.$EXT" | xargs dos2unix
+  find . -type f -iname "*.$EXT" | xargs dos2unix -q
 done
