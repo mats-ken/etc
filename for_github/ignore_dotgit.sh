9dc9065d3258b17b352d5f26c0862e36f7ef09ee
mats-ken <matsken.cpp@gmail.com>
2018-10-11 12:35:28 +0900

    Modified   for_github/ignore_dotgit.sh

--- for_github/ignore_dotgit.sh
+++ for_github/ignore_dotgit.sh
@@ -1,5 +1,5 @@
 cd `dirname $0`
 
 
-find . -maxdepth 1 -type d | grep / | xargs svn add -N
-find . -maxdepth 1 -type d | grep / | xargs -n 1 svn propset svn:ignore .git
+find . -maxdepth 1 -type d | grep / | grep -v ./.svn | xargs svn add -N
+find . -maxdepth 1 -type d | grep / | grep -v ./.svn | xargs -n 1 svn propset svn:ignore .git
